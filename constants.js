4faa26eff80c3eaa18d72a4bd352d39e83f98bc8
Simon Vegsund <sve034@uit.no>
2024-10-17 23:09:38 +0200

    board now adjust size  based on difficulty level

---
+++ constants.js
@@ -0,0 +1,5 @@
+export const difficulties = {
+	beginner: { rows: 9, columns: 9, boardsize: 81, mines: 10 },
+	intermediate: { rows: 16, columns: 16, boardsize: 256, mines: 40 },
+	expert: { rows: 16, columns: 30, boardsize: 480, mines: 99 },
+};
--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 import { compose } from 'lodash/fp';
+import { difficulties } from './constants';
 
 function createTwoDArray(rows = 9, cols = 9) {
-	const boardArray = [];
-
-	for (let row = 0; row < rows; row++) {
-		boardArray[row] = [];
-		for (let col = 0; col < cols; col++) {
-			boardArray[row][col] = row * rows + col;
-		}
+	const tiles = document.querySelectorAll('.tile');
+	const nodeListArray = Array.from(tiles);
+	const twoDArray = [];
+	const arrayLength = nodeListArray.length;
+	for (let index = 0; index < arrayLength; index++) {
+		nodeListArray[index].dataset.index = index;
 	}
-	return boardArray;
+	for (let i = 0; i < arrayLength; i += 9) {
+		twoDArray.push(nodeListArray.slice(i, i + cols));
+	}
+	return twoDArray;
 }
-
-const gameBoard = createTwoDArray();
-console.table(gameBoard);
-
-const tiles = Array.from(document.querySelectorAll('.tile'));
 
 document.addEventListener('contextmenu', (e) => {
 	if (!e.target.closest('.tile')) return;
@@ -27,3 +25,25 @@ document.addEventListener('contextmenu', (e) => {
 	}
 	e.preventDefault();
 });
+
+function createBoard(difficulty = 'beginner') {
+	const boardParent = document.querySelector('.board');
+	const root = document.querySelector(':root');
+	boardParent.innerHTML = '';
+
+	const { rows, columns, boardsize } = difficulties[difficulty];
+	root.style.setProperty('--rows', rows);
+	root.style.setProperty('--cols', columns);
+
+	for (let i = 0; i < boardsize; i++) {
+		const tile = document.createElement('button');
+		tile.classList.add('tile');
+		tile.dataset.revealed = 'false';
+		boardParent.appendChild(tile);
+	}
+}
+createBoard();
+const gameBoard = createTwoDArray();
+console.table(gameBoard);
+
+gameBoard[0][0].dataset.status = 'flagged';
