e94b14589717346ceae967a4ac5be6994e1ba914
Simon Vegsund <sve034@uit.no>
2024-10-19 14:18:47 +0200

    added function for placing mines and setting numbers around mines

---
+++ board.js
@@ -0,0 +1,84 @@
+import { difficulties } from './constants';
+
+export function createBoard(difficulty = 'beginner') {
+	const boardParent = document.querySelector('.board');
+	const numberOfMines = document.querySelector('.mine-counter');
+	const root = document.querySelector(':root');
+	boardParent.innerHTML = '';
+
+	const { rows, columns, boardsize, mines } = difficulties[difficulty];
+	root.style.setProperty('--rows', rows);
+	root.style.setProperty('--cols', columns);
+	numberOfMines.textContent = mines;
+
+	for (let i = 0; i < boardsize; i++) {
+		const tile = document.createElement('button');
+		tile.classList.add('tile');
+		tile.dataset.revealed = 'false';
+		tile.dataset.index = i;
+		boardParent.appendChild(tile);
+	}
+
+	const board = createTwoDArray(rows, columns);
+	const tileArray = Array.from(document.querySelectorAll('.tile'));
+	placeMines(mines, boardsize, tileArray);
+	surroundingMines(board, rows, columns);
+	return { board, rows, columns, boardsize, mines };
+}
+
+export function createTwoDArray(rows = 9, cols = 9) {
+	const tiles = document.querySelectorAll('.tile');
+	const nodeListArray = Array.from(tiles);
+	const twoDArray = [];
+	const arrayLength = nodeListArray.length;
+	for (let index = 0; index < arrayLength; index++) {
+		nodeListArray[index].dataset.index = index;
+	}
+	for (let i = 0; i < arrayLength; i += cols) {
+		twoDArray.push(nodeListArray.slice(i, i + cols));
+	}
+	return twoDArray;
+}
+
+export function placeMines(numMines, boardsize, arr) {
+	const mineSet = new Set();
+	while (mineSet.size < numMines) {
+		const mine = randomNumberGenerator(0, boardsize);
+		mineSet.add(mine);
+	}
+	mineSet.forEach((mine) => {
+		arr[mine].dataset.mine = 'true';
+	});
+	return mineSet;
+}
+
+export function surroundingMines(boardArray, rows, cols) {
+	for (let row = 0; row < rows; row++) {
+		for (let col = 0; col < cols; col++) {
+			if (boardArray[row][col].dataset.mine === 'true') {
+				continue;
+			}
+
+			let mineCount = 0;
+
+			for (let i = row - 1; i <= row + 1; i++) {
+				for (let j = col - 1; j <= col + 1; j++) {
+					if (boardArray?.[i]?.[j]?.dataset.mine === 'true') {
+						mineCount++;
+					}
+				}
+			}
+			if (mineCount === 0) {
+				boardArray[row][col].dataset.status = 'empty';
+			} else {
+				boardArray[row][col].dataset.number = `${mineCount}`;
+			}
+		}
+	}
+}
+
+export function randomNumberGenerator(min, max) {
+	const minCeiled = Math.ceil(min);
+	const maxFloored = Math.floor(max);
+	return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
--- script.js
+++ script.js
@@ -1,69 +1,33 @@
-import { compose } from 'lodash/fp';
-import { difficulties } from './constants';
-
-function createTwoDArray(rows = 9, cols = 9) {
-	const tiles = document.querySelectorAll('.tile');
-	const nodeListArray = Array.from(tiles);
-	const twoDArray = [];
-	const arrayLength = nodeListArray.length;
-	for (let index = 0; index < arrayLength; index++) {
-		nodeListArray[index].dataset.index = index;
-	}
-	for (let i = 0; i < arrayLength; i += cols) {
-		twoDArray.push(nodeListArray.slice(i, i + cols));
-	}
-	return twoDArray;
-}
+import { createBoard } from './board';
+let { board, rows, columns, boardsize, mines } = createBoard();
 
 document.addEventListener('click', (e) => {
 	if (!e.target.closest('.tile')) return;
-	if (e.target.dataset.status !== 'flagged') {
+	if (e.target.dataset.status === 'flagged') {
+		return;
+	} else if (e.target.dataset.mine === 'true') {
 		e.target.dataset.revealed = 'true';
+	} else if (e.target.dataset.number) {
+		e.target.dataset.revealed = 'true';
+		e.target.textContent = e.target.dataset.number;
+	} else {
+		return;
 	}
 });
 
 document.addEventListener('contextmenu', (e) => {
 	if (!e.target.closest('.tile')) return;
-	flagMine(e.target);
+	flagTile(e.target);
 	e.preventDefault();
-});
-
-function createBoard(difficulty = 'beginner') {
-	const boardParent = document.querySelector('.board');
-	const numberOfMines = document.querySelector('.mine-counter');
-	const root = document.querySelector(':root');
-	boardParent.innerHTML = '';
-
-	const { rows, columns, boardsize, mines } = difficulties[difficulty];
-	root.style.setProperty('--rows', rows);
-	root.style.setProperty('--cols', columns);
-	numberOfMines.textContent = mines;
-
-	for (let i = 0; i < boardsize; i++) {
-		const tile = document.createElement('button');
-		tile.classList.add('tile');
-		tile.dataset.revealed = 'false';
-		boardParent.appendChild(tile);
-	}
-
-	const tileArray = Array.from(document.querySelectorAll('.tile'));
-	placeMines(mines, boardsize, tileArray);
-}
-
-document.addEventListener('DOMContentLoaded', (e) => {
-	createBoard();
-	const board = createTwoDArray();
 });
 
 document.addEventListener('click', (e) => {
 	if (!e.target.closest('.difficulty')) return;
 	const difficultyLevel = e.target.textContent.toLowerCase();
-	createBoard(difficultyLevel);
-	const { rows, columns } = difficulties[difficulties];
-	const board = createTwoDArray(rows, columns);
+	board = createBoard(difficultyLevel);
 });
 
-function flagMine(target) {
+function flagTile(target) {
 	const numberOfMines = document.querySelector('.mine-counter');
 	let count = Number(numberOfMines.textContent);
 	if (target.dataset.status === 'flagged') {
@@ -77,26 +41,15 @@ function flagMine(target) {
 	}
 }
 
-function placeMines(numMines, boardsize, arr) {
-	const mineSet = new Set();
-	while (mineSet.size < numMines) {
-		const mine = randomNumberGenerator(0, boardsize);
-		mineSet.add(mine);
-	}
-	mineSet.forEach((mine) => {
-		arr[mine].dataset.mine = 'true';
-	});
-}
-
-function randomNumberGenerator(min, max) {
-	const minCeiled = Math.ceil(min);
-	const maxFloored = Math.floor(max);
-	return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
-}
-
 // document.addEventListener('click', (e) => {
 // 	if (!e.target.closest('.win-lose-button')) return;
 // 	window.location.reload();
 // });
 
-function displaySurroundingMines(boardArray) {}
+function calculateIndex(index, columns) {
+	const row = Math.floor(index / columns);
+	const col = index % columns;
+	return [row, col];
+}
+
+function revealEmptyTiles() {}
